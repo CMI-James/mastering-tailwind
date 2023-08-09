924bc11e74c91994fc82ab98d372818cf354d3c3
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-08-09 23:48:02 +0100

    made some edits

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
+const themeToggle = document.getElementById("theme-toggle");
+const container = document.querySelector(".container");
+const loader = document.querySelector(".loader");
 
-  const themeToggle = document.getElementById('theme-toggle');
-  const container = document.querySelector('.container');
-  const loader = document.querySelector('.loader');
+themeToggle.addEventListener("click", () => {
+  const newTheme = container.classList.contains("light-theme")
+    ? "dark-theme"
+    : "light-theme";
 
-  themeToggle.addEventListener('click', () => {
-    // Determine the new theme based on the current container's class
-    const newTheme = container.classList.contains('light-theme') ? 'dark-theme' : 'light-theme';
-    
-    // Apply loader styles based on the new theme
-    loader.classList.remove('hidden');
-    if (newTheme === 'dark-theme') {
-      loader.style.backgroundColor = 'black';
-      loader.style.color = 'white';
-    } else {
-      loader.style.backgroundColor = 'white';
-      loader.style.color = 'black';
-    }
-    
+  loader.classList.remove("hidden");
+  if (newTheme === "dark-theme") {
+    loader.style.backgroundColor = "black";
+    loader.style.color = "white";
+  } else {
+    loader.style.backgroundColor = "white";
+    loader.style.color = "black";
+  }
 
-    // Add a short delay before toggling the theme class
+  setTimeout(() => {
+    container.classList.toggle("dark-theme");
+    container.classList.toggle("light-theme");
+
     setTimeout(() => {
-      container.classList.toggle('dark-theme');
-      container.classList.toggle('light-theme');
-
-      // Hide loader after theme transition
-      setTimeout(() => {
-        loader.classList.add('hidden');
-      }, 300); // Match the transition duration
-    }, 300); // Match the transition duration
-  });
-
+      loader.classList.add("hidden");
+    }, 400);
+  }, 400);
+});
