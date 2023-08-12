0de26750635ff57741ffd192c1f2c8b9d70cdc10
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-08-13 00:17:11 +0100

    i changed the toggle from an A to be toggle button

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const themeToggle = document.getElementById("theme-toggle");
-const container = document.querySelector(".container");
-const loader = document.querySelector(".loader");
+const main = document.querySelector(".main");
 
 themeToggle.addEventListener("click", () => {
-  const newTheme = container.classList.contains("light-theme")
+  const newTheme = main.classList.contains("light-theme")
     ? "dark-theme"
     : "light-theme";
 
-  loader.classList.remove("hidden");
-  if (newTheme === "dark-theme") {
-    loader.style.backgroundColor = "black";
-    loader.style.color = "white";
-  } else {
-    loader.style.backgroundColor = "white";
-    loader.style.color = "black";
-  }
-
   setTimeout(() => {
-    container.classList.toggle("dark-theme");
-    container.classList.toggle("light-theme");
-
-    setTimeout(() => {
-      loader.classList.add("hidden");
-    }, 400);
-  }, 400);
+    main.classList.toggle("dark-theme");
+    main.classList.toggle("light-theme");
+  }, 100);
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        typing: "typing 5s infinite steps(34)",
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0",
+          },
+          "80%": {
+            width: "34ch",
+          },
+          "100%": {
+            width: "34ch",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
