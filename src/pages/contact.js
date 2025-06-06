d7a14f0f4a3eec51878e2c06a4f869bdfb4c2cf4
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-06-06 09:26:22 +0100

    migrating the work to typescript

--- src/pages/contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const contact = () => {
-  return (
-    <div className=" h-full flex justify-center items-center text-6xl">Contact</div>
-  )
-}
-
-export default contact
---
+++ src/pages/contact.tsx
@@ -0,0 +1,7 @@
+import type React from "react"
+
+const Contact: React.FC = () => {
+  return <div className="h-full flex justify-center items-center text-6xl">Contact</div>
+}
+
+export default Contact
